013cc2692c56304e4e19d70ffae3d97cdf6a010f
huage2580 <2838666797@qq.com>
2024-02-01 10:51:25 +0800

    Initial Commit

---
+++ howToNavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  howToNavigation
+//
+//  Created by hua sheng lu on 2024/2/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ howToNavigation/howToNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  howToNavigationApp.swift
+//  howToNavigation
+//
+//  Created by hua sheng lu on 2024/2/1.
+//
+
+import SwiftUI
+
+@main
+struct howToNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
